e159392e8b1a74e28307cf2db530451f81054316
Ritesh Siotiya <ritesh.siotiya@bookmyshow.com>
2017-11-15 17:25:16 +0530

    first commit

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect(amqpUrl, { heartbeat: 60 }, function (err, conn) {
 
-//amqp.connect(amqpUrl, function(err, conn){
- conn.createConfirmChannel(function(err,ch) {
+	conn.createConfirmChannel(function(err,ch) {
+		
+		var q = 'test123';  
+ 		var content = "hello ritesh";
 
- 	var q = 'test123';  
- 	var content = "hello ritesh";
+ 		try {
 
-	try {
-
-	 	ch.publish("", q, new Buffer("work work work"), { persistent: true },function(err, ok) {
+ 			ch.publish("", q, new Buffer("work work work"), { persistent: true },function(err, ok) {
 	     	if (err) {
 	     		console.error("[AMQP] publish", err);
 	     		ch.connection.close();
 	     	}
 	     });
-     } catch (e) {
+ 		} 
+ 		catch (e) {
 
-     	console.error("[AMQP] publish", e.message);
-     	
-     }
-    
-});
+ 			console.error("[AMQP] publish", e.message);
+ 		}
+ 	});
 }); 
